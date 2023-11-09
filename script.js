538b72d7eb66dcde827e2253d1af6c1fdc10ae53
robbdouglas <robert.nimmrich@icloud.com>
2023-11-09 13:10:39 +0100

    add final ranking and restart button

--- script.js
+++ script.js
@@ -30,223 +30,223 @@ let questions = [
     ["Hermione", "Lavender", "Parvati", "Cho"],
     "Lavender"
   ),
-  new Question(
-    "Harry Potter",
-    300,
-    "The spell 'Sectumsempra' is known for causing deep and slashing wounds. Who was the unintended victim of this spell, cast by Harry Potter?",
-    ["Professor Snape", "Hagrid", "Draco Malfoy", "Luna Lovegood"],
-    "Draco Malfoy"
-  ),
-  new Question(
-    "Harry Potter",
-    400,
-    "In the Room of Requirement, what does Dumbledore's Army use to practice spells and hexes?",
-    [
-      "The Mirror of Erised",
-      "The Pensieve",
-      "The Vanishing Cabinet",
-      "The Dummy Death Eaters",
-    ],
-    "The Dummy Death Eaters"
-  ),
-  new Question(
-    "Harry Potter",
-    500,
-    "In the Triwizard Tournament, what spell did Viktor Krum use to transfigure a shark during the Second Task?",
-    [
-      "Hydrosquamenta",
-      "Aquamutatio Selachimorpha",
-      "Gelum Solum",
-      "Ferula Aquae",
-    ],
-    "Gelum Solum"
-  ),
-  new Question(
-    "Berlin",
-    100,
-    "What is the name of the river that flows through Berlin?",
-    ["Spree", "Elbe", "Rhine", "Danube"],
-    "Spree"
-  ),
-  new Question(
-    "Berlin",
-    200,
-    "Which of the following gates (Tore) does not exist in Berlin?",
-    ["Brandenburger Tor", "Münchner Tor", "Kottbusser Tor", "Schlesisches Tor"],
-    "Münchner Tor"
-  ),
-  new Question(
-    "Berlin",
-    300,
-    "Which district (Bezirk) does not exist in Berlin?",
-    ["Mitte", "Prenzlauer Berg", "Charlottenburg", "Maxvorstadt"],
-    "Maxvorstadt"
-  ),
-  new Question(
-    "Berlin",
-    400,
-    "Which is the subway line between main station (Hauptbahnhof) and Hönow?",
-    ["U1", "U5", "U6", "U8"],
-    "U5"
-  ),
-  new Question(
-    "Berlin",
-    500,
-    "Which area is located in Friedrichshain and is famous for its nightclubs, bars, flee markets and several cultural events?",
-    ["RAW-Gelände", "Mauerpark", "Treptower Park", "Tempelhofer Feld"],
-    "RAW-Gelände"
-  ),
-  new Question(
-    "Web Dev",
-    100,
-    "What does HTML stand for?",
-    [
-      "Hyper Text Makeup Language",
-      "Hyperlinks and Text Markup Language",
-      "Home Tool Markup Language",
-      "Hyper Text Markup Language",
-    ],
-    "Hyper Text Markup Language"
-  ),
-  new Question(
-    "Web Dev",
-    200,
-    "Which of the following programming languages is not a common web development language?",
-    ["JavaScript", "Delphi", "CSS", "HTML"],
-    "Delphi"
-  ),
-  new Question(
-    "Web Dev",
-    300,
-    "Which of the following HTML tags does need a closing tag?",
-    ["p", "img", "div", "section"],
-    "img"
-  ),
-  new Question(
-    "Web Dev",
-    400,
-    "What is the correct way to create an animation in CSS?",
-    [
-      "@keyframes myAnimation { ... }",
-      "@animation myAnimation { ... }",
-      "@animate myAnimation { ... }",
-      "@animate-keyframes myAnimation { ... }",
-    ],
-    "@keyframes myAnimation { ... }"
-  ),
-  new Question(
-    "Web Dev",
-    500,
-    "What is the significance of the keyword 'this' in JavaScript?",
-    [
-      "It refers to the current working directory of the script",
-      "It refers to the previous object",
-      "It represents the object on which the current function is invoked",
-      "It is an array method to iterate over the current object",
-    ],
-    "It represents the object on which the current function is invoked"
-  ),
-  new Question(
-    "Idioms",
-    100,
-    "What does the idiom 'to be over the moon' mean?",
-    [
-      "to be very happy",
-      "to be very sad",
-      "to be very angry",
-      "to be very tired",
-    ],
-    "to be very happy"
-  ),
-  new Question(
-    "Idioms",
-    200,
-    "What does the idiom 'to burn bridges' mean?",
-    [
-      "to destroy bridges",
-      "to ruin relationships",
-      "to attempt something impossible",
-      "to change your job",
-    ],
-    "to ruin relationships"
-  ),
-  new Question(
-    "Idioms",
-    300,
-    "What does the idiom 'You're barkin' up the wrong tree' mean?",
-    [
-      "To be mistaken",
-      "To be lost in the woods",
-      "To be barking at a tree",
-      "To be searching for something in the wrong place",
-    ],
-    "To be searching for something in the wrong place"
-  ),
-  new Question(
-    "Idioms",
-    400,
-    "What does the idiom 'burn the midnight oil' mean?",
-    [
-      "Working late into the night",
-      "Setting something on fire",
-      "Staying up to watch a late-night show",
-      "Partying all night long",
-    ],
-    "Working late into the night"
-  ),
-  new Question(
-    "Idioms",
-    500,
-    "What does the idiom 'to jump on the bandwagon' mean?",
-    [
-      "Criticize a popular trend",
-      "Join others in doing something fashionable",
-      "Form a new musical group",
-      "Start a parade",
-    ],
-    "Join others in doing something fashionable"
-  ),
-  new Question(
-    "Video Games",
-    100,
-    " In the game The Legend of Zelda, what is the protagonist's name?",
-    ["Zelda", "Ganondorf", "Link", "Epona"],
-    "Link"
-  ),
-  new Question(
-    "Video Games",
-    200,
-    "Which of the following video games is not an ego shooter?",
-    ["Call of Duty", "Counter Strike", "League of Legends", "Borderlands"],
-    "League of Legends"
-  ),
-  new Question(
-    "Video Games",
-    300,
-    "Which game is the best-selling video game of all time?",
-    ["Minecraft", "Tetris", "GTA V", "Wii Sports"],
-    "Minecraft"
-  ),
-  new Question(
-    "Video Games",
-    400,
-    "Which of the following Pokémon is not a ground type?",
-    ["Groudon", "Garchomp", "Golem", "Giratina"],
-    "Giratina"
-  ),
-  new Question(
-    "Video Games",
-    500,
-    "Which of the folliwing games is not a Paper Mario Game?",
-    [
-      "Paper Mario: The Thousand-Year Door",
-      "Paper Mario: Sticker Star",
-      "Paper Mario: The Origami King",
-      "Paper Mario 64",
-    ],
-    "Paper Mario 64"
-  ),
-];
+   new Question(
+     "Harry Potter",
+     300,
+     "The spell 'Sectumsempra' is known for causing deep and slashing wounds. Who was the unintended victim of this spell, cast by Harry Potter?",
+     ["Professor Snape", "Hagrid", "Draco Malfoy", "Luna Lovegood"],
+     "Draco Malfoy"
+   ),
+   new Question(
+     "Harry Potter",
+     400,
+     "In the Room of Requirement, what does Dumbledore's Army use to practice spells and hexes?",
+     [
+       "The Mirror of Erised",
+       "The Pensieve",
+       "The Vanishing Cabinet",
+       "The Dummy Death Eaters",
+     ],
+     "The Dummy Death Eaters"
+   ),
+   new Question(
+     "Harry Potter",
+     500,
+     "In the Triwizard Tournament, what spell did Viktor Krum use to transfigure a shark during the Second Task?",
+     [
+       "Hydrosquamenta",
+       "Aquamutatio Selachimorpha",
+       "Gelum Solum",
+       "Ferula Aquae",
+     ],
+     "Gelum Solum"
+   ),
+   new Question(
+     "Berlin",
+     100,
+     "What is the name of the river that flows through Berlin?",
+     ["Spree", "Elbe", "Rhine", "Danube"],
+     "Spree"
+   ),
+   new Question(
+     "Berlin",
+     200,
+     "Which of the following gates (Tore) does not exist in Berlin?",
+     ["Brandenburger Tor", "Münchner Tor", "Kottbusser Tor", "Schlesisches Tor"],
+     "Münchner Tor"
+   ),
+   new Question(
+     "Berlin",
+     300,
+     "Which district (Bezirk) does not exist in Berlin?",
+     ["Mitte", "Prenzlauer Berg", "Charlottenburg", "Maxvorstadt"],
+     "Maxvorstadt"
+   ),
+   new Question(
+     "Berlin",
+     400,
+     "Which is the subway line between main station (Hauptbahnhof) and Hönow?",
+     ["U1", "U5", "U6", "U8"],
+     "U5"
+   ),
+   new Question(
+     "Berlin",
+     500,
+     "Which area is located in Friedrichshain and is famous for its nightclubs, bars, flee markets and several cultural events?",
+     ["RAW-Gelände", "Mauerpark", "Treptower Park", "Tempelhofer Feld"],
+     "RAW-Gelände"
+   ),
+   new Question(
+     "Web Dev",
+     100,
+     "What does HTML stand for?",
+     [
+       "Hyper Text Makeup Language",
+       "Hyperlinks and Text Markup Language",
+       "Home Tool Markup Language",
+       "Hyper Text Markup Language",
+     ],
+     "Hyper Text Markup Language"
+   ),
+   new Question(
+     "Web Dev",
+     200,
+     "Which of the following programming languages is not a common web development language?",
+     ["JavaScript", "Delphi", "CSS", "HTML"],
+     "Delphi"
+   ),
+   new Question(
+     "Web Dev",
+     300,
+     "Which of the following HTML tags does need a closing tag?",
+     ["p", "img", "div", "section"],
+     "img"
+   ),
+   new Question(
+     "Web Dev",
+     400,
+     "What is the correct way to create an animation in CSS?",
+     [
+       "@keyframes myAnimation { ... }",
+       "@animation myAnimation { ... }",
+       "@animate myAnimation { ... }",
+       "@animate-keyframes myAnimation { ... }",
+     ],
+     "@keyframes myAnimation { ... }"
+   ),
+   new Question(
+     "Web Dev",
+     500,
+     "What is the significance of the keyword 'this' in JavaScript?",
+     [
+       "It refers to the current working directory of the script",
+       "It refers to the previous object",
+       "It represents the object on which the current function is invoked",
+       "It is an array method to iterate over the current object",
+     ],
+     "It represents the object on which the current function is invoked"
+   ),
+   new Question(
+     "Idioms",
+     100,
+     "What does the idiom 'to be over the moon' mean?",
+     [
+       "to be very happy",
+       "to be very sad",
+       "to be very angry",
+       "to be very tired",
+     ],
+     "to be very happy"
+   ),
+   new Question(
+     "Idioms",
+     200,
+     "What does the idiom 'to burn bridges' mean?",
+     [
+       "to destroy bridges",
+       "to ruin relationships",
+       "to attempt something impossible",
+       "to change your job",
+     ],
+     "to ruin relationships"
+   ),
+   new Question(
+     "Idioms",
+     300,
+     "What does the idiom 'You're barkin' up the wrong tree' mean?",
+     [
+       "To be mistaken",
+       "To be lost in the woods",
+       "To be barking at a tree",
+       "To be searching for something in the wrong place",
+     ],
+     "To be searching for something in the wrong place"
+   ),
+   new Question(
+     "Idioms",
+     400,
+     "What does the idiom 'burn the midnight oil' mean?",
+     [
+       "Working late into the night",
+       "Setting something on fire",
+       "Staying up to watch a late-night show",
+       "Partying all night long",
+     ],
+     "Working late into the night"
+   ),
+   new Question(
+     "Idioms",
+     500,
+     "What does the idiom 'to jump on the bandwagon' mean?",
+     [
+       "Criticize a popular trend",
+       "Join others in doing something fashionable",
+       "Form a new musical group",
+       "Start a parade",
+     ],
+     "Join others in doing something fashionable"
+   ),
+   new Question(
+     "Video Games",
+     100,
+     " In the game The Legend of Zelda, what is the protagonist's name?",
+     ["Zelda", "Ganondorf", "Link", "Epona"],
+     "Link"
+   ),
+   new Question(
+     "Video Games",
+     200,
+     "Which of the following video games is not an ego shooter?",
+     ["Call of Duty", "Counter Strike", "League of Legends", "Borderlands"],
+     "League of Legends"
+   ),
+   new Question(
+     "Video Games",
+     300,
+     "Which game is the best-selling video game of all time?",
+     ["Minecraft", "Tetris", "GTA V", "Wii Sports"],
+     "Minecraft"
+   ),
+   new Question(
+     "Video Games",
+     400,
+     "Which of the following Pokémon is not a ground type?",
+     ["Groudon", "Garchomp", "Golem", "Giratina"],
+     "Giratina"
+   ),
+   new Question(
+     "Video Games",
+     500,
+     "Which of the folliwing games is not a Paper Mario Game?",
+     [
+       "Paper Mario: The Thousand-Year Door",
+       "Paper Mario: Sticker Star",
+       "Paper Mario: The Origami King",
+       "Paper Mario 64",
+     ],
+     "Paper Mario 64"
+   ),
+ ];
 
 function createPlayerInputs() {
   playerCount = document.getElementById("player-count").value;
@@ -513,10 +513,58 @@ function displayPlayerInfo() {
 }
 
 function endGame() {
-  let winner = players.reduce((prev, current) =>
-    prev.points > current.points ? prev : current
-  );
-  alert(
-    `The game is over! ${winner.name} is the winner with ${winner.points} points! Make some noise! 🎉🎉🎉`
-  );
+  // Sortiere Spieler basierend auf Punkten
+  players.sort((a, b) => b.points - a.points);
+
+  // Verberge alles außer der finalen Box mit Ranking und Restart-Button
+  document.getElementById("start-screen").style.display = "none";
+  document.getElementById("game-board").style.display = "none";
+
+  // Verberge die letzte Frage und ihre Elemente
+  let questionCard = document.querySelector(".question-card");
+  if (questionCard) {
+    questionCard.style.display = "none";
+  }
+
+  // Verberge die Nachrichtenbox und den "Continue"-Button
+  let messageBox = document.querySelector(".message-box");
+  if (messageBox) {
+    messageBox.style.display = "none";
+  }
+
+  let continueButton = document.querySelector(".message-box + button");
+  if (continueButton) {
+    continueButton.style.display = "none";
+  }
+
+  // Erstelle die Final Box für Ranking und Restart-Button
+  let finalBox = document.createElement("div");
+  finalBox.id = "final-box";
+  finalBox.classList.add("transparent-bg", "box-style");
+  finalBox.innerHTML = "<h2>Final Ranking</h2>";
+
+  for (let i = 0; i < players.length; i++) {
+    let rankingInfo = document.createElement("div");
+    rankingInfo.innerHTML = `<p>${i + 1}. ${players[i].name}: ${
+      players[i].points
+    } points</p>`;
+    finalBox.appendChild(rankingInfo);
+  }
+
+  // Setze den Stil für die Final Box
+  finalBox.style.display = "block";
+  document.body.appendChild(finalBox);
+
+  // Füge den Neustart-Button hinzu
+  let restartButton = document.createElement("button");
+  restartButton.id = "restart-button";
+  restartButton.classList.add("transparent-bg", "box-style");
+  restartButton.textContent = "Restart Game";
+  restartButton.onclick = restartGame;
+  document.body.appendChild(restartButton);
+}
+
+function restartGame() {
+  // Aktualisiere die Seite
+  location.reload();
 }
